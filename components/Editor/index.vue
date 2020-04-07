fc78f1431ca90e5e00e5ca713e6c20ee55b11be9
qqdtf98 <qqdtf98@gmail.com>
2020-04-07 17:14:36 +0900

    Get projectdata using project seq

--- components/Editor/index.vue
+++ components/Editor/index.vue
@@ -6,13 +6,26 @@
 </template>
 
 <script lang="ts">
+import { defineComponent, onMounted } from '@vue/composition-api'
 import MenuBar from './MenuBar/index.vue'
 import StylesPanel from './StylesPanel/index.vue'
+import ProjectService from '@/services/project-service'
 
-export default {
+export default defineComponent({
   components: { MenuBar, StylesPanel },
-  setup() {
+  props: {
+    projectId: Number,
+  },
+  setup(props) {
+    onMounted(() => {
+      ProjectService.getProjectData(props.projectId).then((res) => {
+        if (res.data.responseCode === 'SUCCESS') {
+          console.log(res)
+          // TODO set project data at store
+        }
+      })
+    })
     return {}
   },
-}
+})
 </script>
