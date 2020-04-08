10055b2e437abf8e7f814bfc12339e3d86dce227
qqdtf98 <qqdtf98@gmail.com>
2020-04-08 17:50:36 +0900

    Add template icon svg files

---
+++ components/Editor/StudioPanel/StudioTemplate.vue
@@ -0,0 +1,8 @@
+        <block-icon v-if="template === 'block'" class="template-icon" />
+        <image-icon v-if="template === 'image'" class="template-icon" />
+        <link-icon v-if="template === 'link'" class="template-icon" />
+        <text-icon v-if="template === 'text'" class="template-icon" />
+import BlockIcon from '@/components/icons/BlockIcon.vue'
+import ImageIcon from '@/components/icons/ImageIcon.vue'
+import LinkIcon from '@/components/icons/LinkIcon.vue'
+import TextIcon from '@/components/icons/TextIcon.vue'
