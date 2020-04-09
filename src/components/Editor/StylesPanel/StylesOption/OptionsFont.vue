851b6b9e672de8ddb1ecfade7b0d82a6aec861b4
qqdtf98 <qqdtf98@gmail.com>
2020-04-09 13:49:09 +0900

    Add submit font style function

--- src/components/Editor/StylesPanel/StylesOption/OptionsFont.vue
+++ src/components/Editor/StylesPanel/StylesOption/OptionsFont.vue
@@ -2,18 +2,51 @@
   <div id="options-font">
     <div class="font-property-wrapper">
       <div class="font-align-wrapper">
-        <img class="font-align-left" src="@/assets/images/left-align.svg" />
-        <img class="font-align-center" src="@/assets/images/center-align.svg" />
-        <img class="font-align-right" src="@/assets/images/right-align.svg" />
+        <img
+          name="left"
+          class="font-align-left"
+          src="@/assets/images/left-align.svg"
+          @click="submitTextAlign"
+        />
+        <img
+          name="center"
+          class="font-align-center"
+          src="@/assets/images/center-align.svg"
+          @click="submitTextAlign"
+        />
+        <img
+          name="right"
+          class="font-align-right"
+          src="@/assets/images/right-align.svg"
+          @click="submitTextAlign"
+        />
       </div>
       <div class="font-style-wrapper">
-        <img class="font-weight-property" src="@/assets/images/thick.svg" />
-        <img class="font-line-property" src="@/assets/images/line-font.svg" />
-        <img class="font-italic-property" src="@/assets/images/italic.svg" />
-        <img class="font-case-property" src="@/assets/images/bigAndSmall.svg" />
+        <img
+          property="fontWeight"
+          value="bold"
+          class="font-weight-property"
+          src="@/assets/images/thick.svg"
+          @click="submitFontStyle"
+        />
+        <img
+          property="textDecoration"
+          value="underline"
+          class="font-line-property"
+          src="@/assets/images/line-font.svg"
+          @click="submitFontStyle"
+        />
+        <img
+          property="fontStyle"
+          value="italic"
+          class="font-italic-property"
+          src="@/assets/images/italic.svg"
+          @click="submitFontStyle"
+        />
       </div>
     </div>
     <div class="font-attribute-wrapper">
+      <!-- TODO font-family 어떤식으로 변경할 것인지 -->
       <div class="font-family-wrapper">
         <div class="font-family-text">Font Family</div>
         <select id="font-family-select">
@@ -91,6 +124,7 @@ export default defineComponent({
       fontColor: '#fff',
     })
 
+    // default color box를 선택했을 때 changedData 저장
     function submitDefaultValue(e: MouseEvent) {
       let changedData
       const target = e.target
@@ -112,11 +146,36 @@ export default defineComponent({
       }
     }
 
+    // chrome-picker를 선택했을 때 changedData 저장
     function submitPickerValue(color: VueColor) {
       if (vuex.styleData.target) {
         const changedData = {
           style: 'color',
           value: color.hex,
+        }
+        vuex.styleData.SET_CHANGED_DATA(changedData)
+      }
+    }
+
+    // text-align 속성 changedData에 저장
+    function submitTextAlign(e: MouseEvent) {
+      const target = e.target as HTMLElement
+      if (vuex.styleData.target) {
+        const changedData = {
+          style: 'textAlign',
+          value: target.getAttribute('name'),
+        }
+        vuex.styleData.SET_CHANGED_DATA(changedData)
+      }
+    }
+
+    // font-style의 특정 속성 changedData에 저장
+    function submitFontStyle(e: MouseEvent) {
+      const target = e.target as HTMLElement
+      if (vuex.styleData.target) {
+        const changedData = {
+          style: target?.getAttribute('property'),
+          value: target?.getAttribute('value'),
         }
         vuex.styleData.SET_CHANGED_DATA(changedData)
       }
@@ -129,6 +188,8 @@ export default defineComponent({
       submitPickerValue,
       font,
       vuex,
+      submitTextAlign,
+      submitFontStyle,
     }
   },
 })
@@ -149,7 +210,8 @@ export default defineComponent({
     flex-direction: column;
     align-items: center;
     justify-content: center;
-    .font-align-wrapper {
+    .font-align-wrapper,
+    .font-style-wrapper {
       width: 80%;
       height: 3rem;
       position: relative;
@@ -159,31 +221,27 @@ export default defineComponent({
       justify-content: center;
       .font-align-left,
       .font-align-center,
-      .font-align-right {
-        position: absolute;
-        width: 1.7rem;
-        height: 1.7rem;
-      }
-      .font-align-left {
-        left: 0;
-      }
-      .font-align-right {
-        right: 0;
-      }
-    }
-    .font-style-wrapper {
-      width: 100%;
-      height: 3rem;
-      display: grid;
-      grid-template-columns: repeat(4, 1fr);
-      align-items: center;
-      justify-content: center;
+      .font-align-right,
       .font-weight-property,
       .font-line-property,
-      .font-italic-property,
-      .font-case-property {
-        width: 1.7rem;
-        height: 1.7rem;
+      .font-italic-property {
+        position: absolute;
+        width: 2rem;
+        height: 2rem;
+        padding: 0.3rem;
+        border-radius: 0.4rem;
+        cursor: pointer;
+        &:hover {
+          background-color: #646464;
+        }
+      }
+      .font-align-left,
+      .font-weight-property {
+        left: 0;
+      }
+      .font-align-right,
+      .font-italic-property {
+        right: 0;
       }
     }
   }
