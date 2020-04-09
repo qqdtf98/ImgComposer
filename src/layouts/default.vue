b198447033c16cf81f2fe5ac972b106cc5b8f688
qqdtf98 <qqdtf98@gmail.com>
2020-04-09 13:07:45 +0900

    Add vuex module

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -2,6 +2,21 @@
   <nuxt />
 </template>
 
+<script lang="ts">
+import { defineComponent, onMounted } from '@vue/composition-api'
+import { useVuex } from '../modules/vue-hooks'
+import { Vuex } from '@/modules/vuex'
+
+export default defineComponent({
+  setup(...args) {
+    const ctx = args[1]
+    const vuexStore = useVuex(ctx)
+
+    Vuex.init(vuexStore)
+  },
+})
+</script>
+
 <style lang="scss">
 @use '@/assets/styles/global' as *;
 </style>
