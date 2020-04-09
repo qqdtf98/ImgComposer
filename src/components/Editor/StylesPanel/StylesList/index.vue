b267f3b0fe35ca04480991a2ae6c90e9eaf17725
qqdtf98 <qqdtf98@gmail.com>
2020-04-09 16:21:23 +0900

    Change styles layout and use v-for

--- src/components/Editor/StylesPanel/StylesList/index.vue
+++ src/components/Editor/StylesPanel/StylesList/index.vue
@@ -1,27 +1,14 @@
 <template>
   <div id="layout-options">
-    <div class="layout-list-box">
-      <div class="layout-list">layout</div>
-      <div class="nested">
-        <OptionsLayout />
+    <div v-for="(style, i) in styles" :key="i" class="layout-list-box">
+      <div class="layout-list">
+        <div class="layout-text">{{ style }}</div>
+        <img class="layout-icon" src="@/assets/images/down.svg" />
       </div>
-    </div>
-    <div class="layout-list-box">
-      <div class="layout-list">background</div>
       <div class="nested">
-        <OptionsBackground />
-      </div>
-    </div>
-    <div class="layout-list-box">
-      <div class="layout-list">font</div>
-      <div class="nested">
-        <OptionsFont />
-      </div>
-    </div>
-    <div class="layout-list-box">
-      <div class="layout-list">filter</div>
-      <div class="nested">
-        <OptionsFilter />
+        <OptionsPosition v-if="style === 'Position'" />
+        <OptionsLayout v-if="style === 'Geometry'" />
+        <OptionsBackground v-if="style === 'Background Color'" />
       </div>
     </div>
   </div>
@@ -32,10 +19,17 @@ import { defineComponent, onMounted } from '@vue/composition-api'
 import OptionsFilter from './OptionsFilter.vue'
 import OptionsBackground from './OptionsBackground.vue'
 import OptionsFont from './OptionsFont.vue'
+import OptionsPosition from './OptionsPosition.vue'
 import OptionsLayout from './OptionsLayout.vue'
 
 export default defineComponent({
-  components: { OptionsBackground, OptionsFilter, OptionsFont, OptionsLayout },
+  components: {
+    OptionsBackground,
+    OptionsFilter,
+    OptionsFont,
+    OptionsLayout,
+    OptionsPosition,
+  },
   setup() {
     onMounted(() => {
       const toggler = document.getElementsByClassName('layout-list')
@@ -43,11 +37,18 @@ export default defineComponent({
       let j
       for (i = 0; i < toggler.length; i++) {
         toggler[i].addEventListener('click', function (e) {
-          const target = e.target as HTMLElement
+          let target = e.target as HTMLElement
+          if (target.className !== 'layout-list') {
+            target = (e.target as HTMLElement).closest(
+              '.layout-list'
+            ) as HTMLElement
+          }
+
           if (target.parentElement) {
             if (target.parentElement.children[1] instanceof HTMLElement) {
               target.parentElement.children[1].style.transition =
                 'all 300ms ease'
+              target.children[1].classList.toggle('icon-rotate')
               target.parentElement.children[1].classList.toggle('active')
               for (
                 j = 0;
@@ -64,7 +65,17 @@ export default defineComponent({
       }
     })
 
-    return {}
+    const styles: string[] = [
+      'Position',
+      'Display',
+      'Geometry',
+      'Background Color',
+      'Border',
+    ]
+
+    return {
+      styles,
+    }
   },
 })
 </script>
@@ -75,19 +86,35 @@ export default defineComponent({
 #layout-options {
   .layout-list-box {
     text-align: left;
-    // margin: 0.3rem;
-    border-top: 1px solid #333538;
-    border-bottom: 1px solid #333538;
+    border-bottom: 1px solid #000000;
     padding: 0.5rem;
+    min-height: 50px;
+
     .layout-list {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content: center;
       margin-left: 0.4rem;
+      min-height: 30px;
+      position: relative;
       @include auto-text-color;
       cursor: pointer;
+      letter-spacing: 0.3px;
+      font-weight: 450;
       user-select: none; /* Prevent text selection */
       transition: all 300ms ease;
+      .layout-text {
+        position: absolute;
+        left: 0;
+      }
+      .layout-icon {
+        width: 1rem;
+        position: absolute;
+        right: 0;
+      }
     }
     .layout-list::before {
-      content: '\FF1E';
       @include auto-text-color;
       display: inline-block;
       margin-right: 6px;
@@ -105,6 +132,9 @@ export default defineComponent({
       justify-content: center;
       flex-direction: column;
       align-items: center;
+    }
+    .icon-rotate {
+      transform: rotate(180deg);
     }
     .template {
       @include auto-text-color;
