ae188cdd6a699ffb40d90a884046b3a40be47313
qqdtf98 <qqdtf98@gmail.com>
2020-04-13 16:20:17 +0900

    Add context and cssom for css selector

--- src/components/Editor/Canvas/Context/index.vue
+++ src/components/Editor/Canvas/Context/index.vue
@@ -1,6 +1,28 @@
+<template>
+  <div id="css-rules">
+    <div class="rules-title">CSS Rules</div>
+    <div class="selector-list-wrapper">
+      <div
+        v-for="(rule, i) in cssRules"
+        :key="i"
+        class="selector-list"
+        @click="selectCssSelector($event, rule)"
+      >
+        {{ rule.selectorText }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, watch, ref } from '@vue/composition-api'
 import { useVuex } from '../../../../modules/vue-hooks'
 import { Cem } from '@/modules/custom-events-manager'
+
+export default defineComponent({
+  setup(props, ctx) {
     const vuex = useVuex(ctx)
+    const cssRules = ref<CSSStyleRule[]>(null)
     watch(
       () => vuex.editorInfo.matchedCssRules,
       () => {
@@ -17,8 +39,51 @@ import { Cem } from '@/modules/custom-events-manager'
         Cem.dispatchEvent('openstyles')
       }, 0)
     }
-    }
 
+    return {
+      cssRules,
+      selectCssSelector,
     }
+  },
+})
+</script>
 
+<style lang="scss">
+#css-rules {
+  background-color: rgba(#3a3a3c, 0.8);
+  backdrop-filter: blur(20px) saturate(1.7);
+  width: 10rem;
+  border-radius: 0.5rem;
+  overflow: hidden;
+  position: fixed;
+  padding-bottom: 0.2rem;
+
+  .rules-title {
+    font-size: 1.1rem;
+    width: 100%;
+    padding-top: 0.5rem;
+    padding-bottom: 0.5rem;
+    padding-left: 0.7rem;
+    padding-right: 0.7rem;
+  }
+
+  .selector-list-wrapper {
+    width: 100%;
+
+    .selector-list {
+      width: 100%;
+      padding-top: 0.25rem;
+      padding-bottom: 0.25rem;
+      padding-right: 0.7rem;
+      padding-left: 0.7rem;
+      font-size: 0.9rem;
+      border-top: 1.5px solid black;
+
+      &:hover {
+        background-color: #5a5a5e;
+        cursor: pointer;
+      }
     }
+  }
+}
+</style>
