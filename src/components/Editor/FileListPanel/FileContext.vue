fbd108ee0df7ab97164f832a5b59bd52184ac61e
qqdtf98 <qqdtf98@gmail.com>
2020-04-17 14:04:24 +0900

    feat: add fileContext and folderContext

---
+++ src/components/Editor/FileListPanel/FileContext.vue
@@ -0,0 +1,67 @@
+<template>
+  <div id="file-context">
+    <div class="delete-file-menu" @click="deleteFile">
+      <img class="delete-file-icon" src="@/assets/images/deleteFile.svg" />
+      <div class="delete-file-text">삭제</div>
+    </div>
+    <div class="rename-file-menu">
+      <img class="rename-file-icon" src="@/assets/images/renameFile.svg" />
+      <div class="rename-file-text">이름 변경</div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, watchEffect } from '@vue/composition-api'
+import FileService from '@/services/file-service.ts'
+
+export default defineComponent({
+  setup(props, ctx) {
+    watchEffect(() => {
+      console.log(props.fileId)
+    })
+
+  },
+})
+</script>
+
+<style lang="scss">
+@use '@/assets/styles/package' as *;
+
+#file-context {
+  @include auto-bg-color;
+  position: fixed;
+  border-radius: 0.3rem;
+  .delete-file-menu,
+  .rename-file-menu {
+    width: 110px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: left;
+    padding: 0.4rem 0.6rem 0.4rem 0.6rem;
+    &:hover {
+      background-color: #525252bd;
+      border-radius: 0.3rem;
+    }
+
+    .delete-file-icon,
+    .rename-file-icon {
+      margin-right: 0.5rem;
+      width: 17px;
+      user-select: none;
+    }
+    .delete-file-text,
+    .rename-file-text {
+      font-size: 0.9rem;
+      cursor: default;
+    }
+    .delete-file-text {
+      color: red;
+    }
+  }
+  .rename-file-menu {
+    border-top: 1px solid black;
+  }
+}
+</style>
--- src/components/Editor/FileListPanel/FileList.vue
+++ src/components/Editor/FileListPanel/FileList.vue
@@ -3,29 +3,50 @@
     <div class="file-path">
       Root >
     </div>
-    <div class="project-data-list">
+
+    <vue-custom-scrollbar class="file-scroll-area">
       <div
-        v-for="(file, i) in vuex.fileData.fileList"
-        :key="i"
-        class="project-data-wrapper"
-        @dblclick="loadFile($event, file)"
+        class="project-data-list"
+        @contextmenu="openFolderContext"
+        @click="closeFileContext"
       >
-        <img class="project-icon" src="@/assets/images/fileIcon.svg" />
-        <div class="project-data">
-          {{ `${file.fileName}.${file.fileType}` }}
+        <div
+          v-for="(file, i) in vuex.fileData.fileList"
+          :key="i"
+          class="project-data-wrapper"
+          @dblclick="loadFile($event, file)"
+          @contextmenu="openFileContext($event, file.fileId)"
+        >
+          <img class="project-icon" src="@/assets/images/fileIcon.svg" />
+          <div class="project-data">
+            {{ `${file.fileName}.${file.fileType}` }}
+          </div>
         </div>
       </div>
-    </div>
+    </vue-custom-scrollbar>
+    <FolderContext v-show="activateContext === 'folder'" ref="folderRef" />
+    <FileContext
+      v-show="activateContext === 'file'"
+      ref="fileRef"
+      :fileId="selectedFile"
+      :closeFileContext="closeFileContext"
+    />
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, watch } from '@vue/composition-api'
-import { useVuex } from '../../../modules/vue-hooks'
-import { Cem } from '../../../modules/custom-events-manager'
+import { defineComponent, watch, onMounted, ref } from '@vue/composition-api'
+import vueCustomScrollbar from 'vue-custom-scrollbar'
+import { useVuex } from '@/modules/vue-hooks'
+import { Cem } from '@/modules/custom-events-manager'
 import { File } from '@/interfaces/any-editor-file'
+import { actionTree } from 'nuxt-typed-vuex'
+import FolderContext from './FolderContext.vue'
+import FileContext from './FileContext.vue'
+import { VueComponent } from '@/types/vue-component'
 
 export default defineComponent({
+  components: { vueCustomScrollbar, FolderContext, FileContext },
   setup(props, ctx) {
     const vuex = useVuex(ctx)
 
@@ -33,9 +54,88 @@ export default defineComponent({
       vuex.fileData.SET_SELECTED_FILE(selectedFile)
     }
 
+    onMounted(() => {
+      window.addEventListener('contextmenu', (e) => {
+        e.preventDefault()
+      })
+    })
+
+    const activateContext = ref('')
+
+    const folderRef = ref<VueComponent>(null)
+
+    const fileRef = ref<VueComponent>(null)
+
+    // calculate Context position
+    function placeContext(elem: HTMLElement, e: MouseEvent) {
+      if (
+        e.clientX + elem.getBoundingClientRect().width > window.innerWidth &&
+        e.clientY + elem.getBoundingClientRect().height < window.innerHeight
+      ) {
+        elem.style.left =
+          window.innerWidth - elem.getBoundingClientRect().width + 'px'
+        elem.style.top = e.clientY + 'px'
+      } else if (
+        e.clientX + elem.getBoundingClientRect().width < window.innerWidth &&
+        e.clientY + elem.getBoundingClientRect().height > window.innerHeight
+      ) {
+        elem.style.left = e.clientX + 'px'
+        elem.style.top =
+          window.innerHeight - elem.getBoundingClientRect().height + 'px'
+      } else if (
+        e.clientX + elem.getBoundingClientRect().width > window.innerWidth &&
+        e.clientY + elem.getBoundingClientRect().height > window.innerHeight
+      ) {
+        elem.style.left =
+          window.innerWidth - elem.getBoundingClientRect().width + 'px'
+        elem.style.top =
+          window.innerHeight - elem.getBoundingClientRect().height + 'px'
+      } else {
+        elem.style.left = e.clientX + 'px'
+        elem.style.top = e.clientY + 'px'
+      }
+    }
+
+    const selectedFile = ref(0)
+
+    // open fileContext and set props by fileId
+    function openFileContext(e: MouseEvent, fileId: number) {
+      activateContext.value = 'file'
+      const File = fileRef.value?.$el as HTMLElement
+      placeContext(File, e)
+      selectedFile.value = fileId
+    }
+
+    // open FolderContext
+    function openFolderContext(e: MouseEvent) {
+      const target = e.target as HTMLElement
+      if (target.className === 'project-data-list') {
+        activateContext.value = 'folder'
+        const Folder = folderRef.value?.$el as HTMLElement
+        placeContext(Folder, e)
+      }
+    }
+
+    // close Context
+    function closeFileContext() {
+      if (
+        activateContext.value === 'folder' ||
+        activateContext.value === 'file'
+      ) {
+        activateContext.value = 'false'
+      }
+    }
+
     return {
       vuex,
       loadFile,
+      openFileContext,
+      activateContext,
+      folderRef,
+      fileRef,
+      closeFileContext,
+      selectedFile,
+      openFolderContext,
     }
   },
 })
@@ -50,6 +150,7 @@ export default defineComponent({
   justify-content: center;
   position: absolute;
   top: 45px;
+  padding-bottom: 1rem;
 
   .file-path {
     margin-top: 0.3rem;
@@ -63,23 +164,35 @@ export default defineComponent({
     font-size: 0.95rem;
   }
 
-  .project-data-list {
-    width: 85%;
+  .file-scroll-area {
+    padding: 0 15px;
+    width: 100%;
+    height: 200px;
+    .project-data-list {
+      width: 95%;
+      padding-bottom: 2rem;
 
-    .project-data-wrapper {
-      display: flex;
-      flex-direction: row;
-      margin-top: 0.4rem;
-      margin-bottom: 0.4rem;
-      cursor: pointer;
-      user-select: none;
-
-      .project-icon {
-        margin-right: 0.5rem;
-      }
-
-      .project-data {
+      .project-data-wrapper {
+        display: flex;
+        flex-direction: row;
+        padding-top: 0.3rem;
+        padding-bottom: 0.3rem;
+        padding-left: 0.4rem;
         width: 100%;
+        border-radius: 0.4rem;
+        cursor: pointer;
+        user-select: none;
+        &:hover {
+          background-color: #5858589d;
+        }
+
+        .project-icon {
+          margin-right: 0.5rem;
+        }
+
+        .project-data {
+          width: 100%;
+        }
       }
     }
   }
