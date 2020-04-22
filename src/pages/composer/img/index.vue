f5b9b42be55c9498770e2b8f06ed777ea7c3f25c
qqdtf98 <qqdtf98@gmail.com>
2020-04-22 14:02:51 +0900

    feat: create composer page

---
+++ src/pages/composer/img/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <ImgMode />
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api'
+import ImgMode from '@/components/Composer/ImgMode/index.vue'
+
+export default defineComponent({
+  components: { ImgMode },
+})
+</script>
