432d89702faab362d82c8400aa2dca899c4b9a97
qqdtf98 <qqdtf98@gmail.com>
2020-04-24 14:25:09 +0900

    feat: create identifier with component data

--- src/components/Composer/ImgMode/ImgLoad.vue
+++ src/components/Composer/ImgMode/ImgLoad.vue
@@ -1,16 +1,7 @@
 <template>
-  <div>
-    <div ref="sampleRef" @mousedown="drawSelector">
-      <div
-        ref="imgLoadRef"
-        style="
-          border: 1px solid black;
-          width: 1000px;
-          height: 700px;
-          user-select: none;
-          pointer-events: none;
-        "
-      >
+  <div id="img-load">
+    <div ref="sampleRef" class="img-load-box" @mousedown="drawSelector">
+      <div ref="imgLoadRef" class="preview-wrapper">
         <img
           id="preview"
           src
@@ -18,15 +9,43 @@
           alt="로컬에 있는 이미지가 보여지는 영역"
         />
       </div>
+      <input id="getfile" type="file" accept="image/*" @change="inputChange" />
+      <div
+        v-for="(id, i) in identifierData"
+        :key="i"
+        class="identifier-wrapper"
+        :style="{
+          left: id.left + 'px',
+          top: id.top + 'px',
+        }"
+      >
+        <Identifier
+          class="component-identifier"
+          :style="{
+            left: id.left + 'px',
+            top: id.top + 'px',
+            width: id.width + 'px',
+            height: id.height + 'px',
+            border: `2px solid ${id.color}`,
+          }"
+        />
+        <IdData
+          v-show="id.state"
+          class="component-data"
+          :style="{ left: id.left + 'px', top: id.top + id.height + 'px' }"
+        />
+      </div>
     </div>
-    <input id="getfile" type="file" accept="image/*" @change="inputChange" />
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, Ref } from '@vue/composition-api'
+import { defineComponent, ref, Ref, reactive } from '@vue/composition-api'
+import Identifier from '@/components/Composer/ImgMode/Identifier.vue'
+import IdData from '@/components/Composer/ImgMode/IdData/index.vue'
 
 export default defineComponent({
+  components: { Identifier, IdData },
   setup() {
     const imgLoadRef = ref<HTMLElement>(null)
 
@@ -46,6 +65,22 @@ export default defineComponent({
 
     const sampleRef = ref() as Ref<HTMLElement>
 
+    type IdentifierType = {
+      left: number
+      top: number
+      width: number
+      height: number
+      color: string
+      calWidth: number
+      calHeight: number
+      calLeft: number
+      calTop: number
+      state: boolean
+    }
+    type Identifiers = IdentifierType[]
+
+    const identifierData = reactive<Identifiers>([])
+
     function drawSelector(e: MouseEvent) {
       const imgElm = document.querySelector('img')
       const imgSrc = imgElm?.src as string
@@ -54,33 +89,37 @@ export default defineComponent({
         return
       }
 
-      const selector = document.createElement('div')
       const initX = e.clientX
       const initY = e.clientY
 
-      selector.style.border = '2px solid black'
-      selector.style.position = 'fixed'
-      selector.style.left = initX + 'px'
-      selector.style.top = initY + 'px'
-      selector.style.width = '1px'
-      selector.style.height = '1px'
-      selector.style.backgroundColor = '#00000066'
-      selector.style.borderRadius = '0.4rem'
-      selector.classList.add('component-selector')
+      identifierData.push({
+        left: initX,
+        top: initY,
+        width: 0,
+        height: 0,
+        color: 'black',
+        calWidth: 0,
+        calHeight: 0,
+        calLeft: 0,
+        calTop: 0,
+        state: false,
+      })
 
-      sampleRef.value.appendChild(selector)
       let moveEvent: (e: MouseEvent) => void
       let upEvent: (e: MouseEvent) => void
+
+      const index = identifierData.length - 1
+
       window.addEventListener(
         'mousemove',
         (moveEvent = (evt: MouseEvent) => {
           const deltaX = evt.clientX - initX
           const deltaY = evt.clientY - initY
 
-          selector.style.left = initX + (deltaX < 0 ? deltaX : 0) + 'px'
-          selector.style.width = Math.abs(deltaX) + 'px'
-          selector.style.top = initY + (deltaY < 0 ? deltaY : 0) + 'px'
-          selector.style.height = Math.abs(deltaY) + 'px'
+          identifierData[index].left = initX + (deltaX < 0 ? deltaX : 0)
+          identifierData[index].top = initY + (deltaY < 0 ? deltaY : 0)
+          identifierData[index].width = Math.abs(deltaX)
+          identifierData[index].height = Math.abs(deltaY)
         })
       )
       window.addEventListener(
@@ -90,17 +129,16 @@ export default defineComponent({
           window.removeEventListener('mouseup', upEvent)
 
           if (!imgLoadRef.value) return
-          const selectorRect = selector.getBoundingClientRect()
           const imgRect = imgLoadRef.value.getBoundingClientRect()
-          const calWidth = selectorRect.width / imgRect.width
-          const calHeight = selectorRect.height / imgRect.height
-          const calLeft = selectorRect.left / imgRect.width
-          const calTop = selectorRect.top / imgRect.height
 
-          console.log(calWidth)
-          console.log(calHeight)
-          console.log(calLeft)
-          console.log(calTop)
+          identifierData[index].calLeft =
+            identifierData[index].left / imgRect.left
+          identifierData[index].calTop = identifierData[index].top / imgRect.top
+          identifierData[index].calWidth =
+            identifierData[index].width / imgRect.width
+          identifierData[index].calHeight =
+            identifierData[index].height / imgRect.height
+          identifierData[index].state = true
         })
       )
     }
@@ -110,7 +148,44 @@ export default defineComponent({
       sampleRef,
       imgLoadRef,
       drawSelector,
+      identifierData,
     }
   },
 })
 </script>
+
+<style lang="scss">
+#img-load {
+  width: 100%;
+  height: calc(100vh - 56px);
+  .img-load-box {
+    width: 100%;
+    height: 100%;
+    .preview-wrapper {
+      width: 100%;
+      height: 100%;
+      border: 1px solid black;
+      user-select: none;
+      pointer-events: none;
+    }
+    #getfile {
+      position: fixed;
+      top: 50%;
+      left: 50%;
+    }
+    .identifier-wrapper {
+      position: fixed;
+
+      .component-identifier {
+        border: 2px solid black;
+        position: fixed;
+        background-color: #00000066;
+        border-radius: 0.4rem;
+      }
+      .component-data {
+        position: fixed;
+      }
+    }
+  }
+}
+</style>
