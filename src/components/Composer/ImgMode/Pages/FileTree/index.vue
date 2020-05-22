5fbde96a3d92736ab403d8514a255a1487d81bc0
qqdtf98 <qqdtf09@gmail.com>
2020-05-22 10:52:04 +0900

    feat: create folder directory and open file

--- src/components/Composer/ImgMode/Pages/FileTree/index.vue
+++ src/components/Composer/ImgMode/Pages/FileTree/index.vue
@@ -25,25 +25,81 @@ export default defineComponent({
       }
     })
 
+    function foldDirectory(e: MouseEvent) {
+      let target = e.target as HTMLElement
+      target = target.closest('.folder-box') as HTMLElement
+      target.children[0].classList.toggle('arrow-spin')
+      if (!target.parentElement) return
+      for (let i = 1; i < target.parentElement.children.length; i++) {
+        target.parentElement.children[i].classList.toggle('folder-display')
+      }
+    }
+
+    function openDirectoryFile(e: MouseEvent) {
+      let target = e.target as HTMLElement
+      target = target.closest('.file') as HTMLElement
+
+      const changePage = vuex.identifier.pages.find(
+        (elem) => elem.imagePath === target.getAttribute('path')
+      )
+      if (!changePage) return
+      const preview = document.querySelector('#preview') as HTMLImageElement
+      preview.src = changePage.imageData
+      const image: NodeListOf<HTMLElement> = document.querySelectorAll(
+        '#page-img'
+      )
+      const imageArr = Array.prototype.slice.call(image)
+      const selectedPageIndex = imageArr.findIndex(
+        (elem) => elem.src === changePage.imageData
+      )
+      vuex.identifier.SET_SELECTED_PAGE_INDEX(selectedPageIndex)
+    }
+
     function createFileTree(directory: DirType, parent: HTMLElement) {
       if (!parent) return
       if (directory.children.length === 0) {
         // File
         const childElement = document.createElement('div')
-        childElement.textContent = 'file ' + directory.name
+        childElement.classList.add('file')
+        childElement.setAttribute(
+          'path',
+          parent.getAttribute('path') + '/' + directory.name
+        )
+        childElement.addEventListener('click', openDirectoryFile)
+        const imgElement = document.createElement('img')
+        imgElement.src = '/images/filegray.svg'
+        imgElement.classList.add('down-img')
+        const textElement = document.createElement('div')
+        textElement.textContent = directory.name
+        childElement.appendChild(imgElement)
+        childElement.appendChild(textElement)
         parent.appendChild(childElement)
       } else {
         // Folder
+        const folderWrapper = document.createElement('div')
+        folderWrapper.classList.add('folder')
+        if (parent.getAttribute('path')) {
+          folderWrapper.setAttribute(
+            'path',
+            parent.getAttribute('path') + '/' + directory.name
+          )
+        } else {
+          folderWrapper.setAttribute('path', directory.name)
+        }
         const folderElement = document.createElement('div')
-        folderElement.classList.add('folder')
+        folderElement.classList.add('folder-box')
+        folderElement.addEventListener('click', foldDirectory)
         const imgElement = document.createElement('img')
-        imgElement.src = '@/assets/images/rightarrow.svg'
+        imgElement.src = '/images/rightarrow.svg'
+        imgElement.classList.add('down-img')
         const textElement = document.createElement('div')
         textElement.textContent = directory.name
-        // folderElement.appendChild(iumg)
-        parent.appendChild(folderElement)
+        folderElement.appendChild(imgElement)
+        folderElement.appendChild(textElement)
+        folderWrapper.appendChild(folderElement)
+        parent.appendChild(folderWrapper)
         for (let i = 0; i < directory.children.length; i++) {
-          createFileTree(directory.children[i], folderElement)
+          createFileTree(directory.children[i], folderWrapper)
         }
       }
     }
@@ -61,7 +117,42 @@ export default defineComponent({
   height: 300px;
 
   .folder {
-    margin-left: 10px;
+    margin-left: 13px;
+
+    .folder-box {
+      display: flex;
+      flex-direction: row;
+      margin: 2px 0;
+      cursor: pointer;
+
+      .down-img {
+        margin-right: 10px;
+        transform: rotate(90deg);
+      }
+
+      .arrow-spin {
+        transform: rotate(0deg);
+      }
+    }
+
+    .file {
+      display: flex;
+      flex-direction: row;
+      padding-left: 13px;
+      margin: 2px 0;
+
+      .down-img {
+        margin-right: 8px;
+      }
+    }
+
+    .folder-display {
+      display: none;
+    }
+  }
+
+  .folder-display {
+    display: none;
   }
 }
 </style>
